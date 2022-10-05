921ccce072fb4069c1eaec8ac21c89723356644d
Ignacio Torres <itorres.developer@gmail.com>
2022-10-04 23:11:36 -0400

    Se agrega Bootstrap 5

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 window._ = _;
-
+import 'bootstrap'
+import * as Popper from '@popperjs/core'
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
