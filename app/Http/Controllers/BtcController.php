8102a4ffa358d6adfadeed29fdf678564c2d4442
Ignacio Torres <itorres.developer@gmail.com>
2022-10-05 15:36:17 -0400

    Porcentajes de Variacion con directivas Blade

---
+++ app/Http/Controllers/BtcController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Btc;
+use Illuminate\Http\Request;
+
+class BtcController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $btc = Btc::all();
+        // $btcAnterio = 
+        return view('welcome', ['btc'=> $btc]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $btc = new Btc;
+        $btc->currency = $request->currency;
+        $btc->rates = $request->rates;
+        $btc->save();
+
+        return response()->json($btc, 200);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Btc  $btc
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Btc $btc)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Btc  $btc
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Btc $btc)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Btc  $btc
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Btc $btc)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Btc  $btc
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Btc $btc)
+    {
+        //
+    }
+}
